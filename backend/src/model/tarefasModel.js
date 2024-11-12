66a36d14a15c27532d083995243c7ee98fd8e549
asnts <annandasants@gmail.com>
2024-11-11 23:56:57 -0300

    update queries

--- backend/src/model/tarefasModel.js
+++ backend/src/model/tarefasModel.js
@@ -1,7 +1,7 @@
 const connection = require('./connection')
 
 const getAll = async () => {
-    const [tarefas] = await connection.execute('SELECT * FROM tarefas');
+    const [tarefas] = await connection.execute('SELECT * FROM Tarefas');
     return tarefas;
 };
 
@@ -9,14 +9,14 @@ const criarTarefa = async (tarefa) => {
     const {nome, custo}  = tarefa;
     const data = new Date(Date.now()).toUTCString();
 
-    const query = 'INSERT INTO tarefas(nome, custo, data) VALUE (?, ?, ?)';
+    const query = 'INSERT INTO Tarefas(nome, custo, data) VALUE (?, ?, ?)';
 
     
 
 };
 
 const deletarTarefa = async (id) => {
-    const [tarefaRemovida] = await connection.execute('DELETE FROM tarefas where id = ?', [id]);
+    const [tarefaRemovida] = await connection.execute('DELETE FROM Tarefas where id = ?', [id]);
      return tarefaRemovida;
 };
 
@@ -24,7 +24,7 @@ const deletarTarefa = async (id) => {
 const atualizarTarefa = async (id, tarefa) => {
     const {nome, custo, data} = tarefa;
 
-   const query = 'UPDATE tarefas SET nome = ?, custo = ?, data = ?, WHERE id = ?';
+   const query = 'UPDATE Tarefas SET nome = ?, custo = ?, data = ?, WHERE id = ?';
    
    const [tarefaAtualizada] = await connection.execute(query, [nome, custo, data, id]);
    return tarefaAtualizada;
