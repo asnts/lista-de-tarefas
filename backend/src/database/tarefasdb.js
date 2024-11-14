eb6d03b135d7c14ddf22416ae296a607f9faaf73
asnts <annandasants@gmail.com>
2024-11-13 22:32:08 -0300

    feat: alteração de conexão de connection para pool

--- backend/src/database/tarefasdb.js
+++ backend/src/database/tarefasdb.js
@@ -1,26 +1,23 @@
 // db.js
-require ('dotenv').config();
+require('dotenv').config();
 const mysql = require('mysql2');
 
-
 // Configuração da conexão
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
 });
 
-
-
-
-// Conectando ao MySQL
-db.connect((err) => {
+// A pool de conexões gerencia as conexões automaticamente, não é necessário chamar `connect()`
+db.getConnection((err, connection) => {
   if (err) {
     console.error('Erro ao conectar ao MySQL:', err);
     return;
   }
   console.log('Conectado ao banco de dados MySQL.');
+  connection.release(); // Libera a conexão de volta para a pool
 });
 
 module.exports = db;
