6992e330eabae44f4364222e0071522163f43ff6
asnts <annandasants@gmail.com>
2024-11-18 23:34:27 -0300

    feat: adjuste variables

--- backend/src/database/tarefasdb.js
+++ backend/src/database/tarefasdb.js
@@ -18,10 +18,11 @@ const db = mysql.createPool({
 
 console.log('Tentando conectar com as seguintes variáveis:');
 console.log({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQL_HOST,  // Verifique se é o nome correto da variável
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 db.getConnection((err, connection) => {
