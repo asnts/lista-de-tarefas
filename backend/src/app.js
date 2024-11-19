3cfc3deb7b75cdec196ed4b75bb5bcecdd4b4bd4
asnts <annandasants@gmail.com>
2024-11-18 23:30:57 -0300

    feat: insert app.get

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,4 +8,14 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
+app.get('/test-db', (req, res) => {
+    db.query('SELECT 1 + 1 AS resultado', (err, results) => {
+      if (err) {
+        console.error('Erro ao testar conexão com o banco:', err);
+        return res.status(500).json({ error: 'Erro ao conectar com o banco' });
+      }
+      res.status(200).json({ resultado: results[0].resultado });
+    });
+  });
+
 module.exports = app;
