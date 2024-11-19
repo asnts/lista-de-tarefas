7a69596e6991bba154cc5065da8da220b3712d08
asnts <annandasants@gmail.com>
2024-11-19 20:20:11 -0300

    feat: Adjust in variables connection

--- backend/src/database/tarefasdb.js
+++ backend/src/database/tarefasdb.js
@@ -1,28 +1,28 @@
 // db.js
 require('dotenv').config();
 const mysql = require('mysql2/promise');
-console.log(process.env.MYSQL_USER);  // Deve retornar 'root'
-console.log(process.env.MYSQL_PASSWORD);  // Deve retornar a senha
-console.log(process.env.MYSQL_DATABASE)
+console.log(process.env.MYSQLUSER);  // Deve retornar 'root'
+console.log(process.env.MYSQLPASSWORD);  // Deve retornar a senha
+console.log(process.env.MYSQLDATABASE)
 
 
 // Configuração da conexão
 const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,  // Verifique se é o nome correto da variável
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  host: process.env.MYSQLHOST,  // Verifique se é o nome correto da variável
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE
 });
 
 
 console.log('Tentando conectar com as seguintes variáveis:');
 console.log({
-  host: process.env.MYSQL_HOST,  // Verifique se é o nome correto da variável
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQLHOST,  // Verifique se é o nome correto da variável
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
 db.getConnection((err, connection) => {
--- backend/src/model/connection.js
+++ backend/src/model/connection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,  // Verifique se é o nome correto da variável
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  host: process.env.MYSQLHOST,  // Verifique se é o nome correto da variável
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE
 });
 
 module.exports = connection;
