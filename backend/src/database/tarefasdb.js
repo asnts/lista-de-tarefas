40408e19cb896ac400a90dbe66e836f34f8cf51e
asnts <annandasants@gmail.com>
2024-11-20 21:23:54 -0300

    feat: remove console.log

--- backend/src/database/tarefasdb.js
+++ backend/src/database/tarefasdb.js
@@ -1,10 +1,6 @@
 // db.js
 require('dotenv').config();
 const mysql = require('mysql2/promise');
-console.log(process.env.MYSQLUSER);  // Deve retornar 'root'
-console.log(process.env.MYSQLPASSWORD);  // Deve retornar a senha
-console.log(process.env.MYSQLDATABASE)
-
 
 // Configuração da conexão
 const db = mysql.createPool({
@@ -15,16 +11,6 @@ const db = mysql.createPool({
   database: process.env.MYSQL_DB,
    });
 
-
-
-console.log('Tentando conectar com as seguintes variáveis:');
-console.log({
-  port:process.env.MYSQL_PORT,
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
-});
 
 db.getConnection((err, connection) => {
   if (err) {
